acc7be76c9e5e290bb6f56a7c67c4906fc916d16
gilsonvalves <gilsonvalves@hotmail.com>
2018-12-26 20:50:43 -0200

    Validacao email nao repetido

--- src/main/java/com/gilsonvalves/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/gilsonvalves/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.gilsonvalves.cursomc.domain.Cliente;
+import com.gilsonvalves.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
